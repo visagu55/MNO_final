471a5fc9751b609a44cfa47a920a16f8b68ac521
Juan Mpc <jbmpc@outlook.com>
2019-05-16 02:02:26 -0500

    agregando funciones auxiliares

---
+++ scripts/funs_auxiliares.R
@@ -0,0 +1,152 @@
+gradiente <- function(f, x) {
+  
+  # In:
+  # f función a la que se le calculará su gradiente
+  # x punto en el que se aproximará el gradiente de f
+  # Out:
+  # grad_f: aproximación al gradiente de f por diferencias centradas
+  
+  h      <- 1e-6
+  n      <- length(x)
+  grad_f <- matrix(rep(0, n), nrow = n)
+  xaux   <- x
+  xaux2  <- x
+  
+  for(i in 1:n) {
+    
+    xaux[i]   <- x[i] + h
+    xaux2[i]  <- x[i] - h
+    grad_f[i] <- (f(xaux) - f(xaux2)) / (2 * h)
+    xaux[i]   <- xaux[i] - h
+    xaux2[i]  <- xaux2[i] + h
+    
+  }
+  
+  return(grad_f)
+  
+}
+
+
+hessiana <- function(f, x) {
+  
+  # In:
+  # f función a la que se le calculará su Hessiana
+  # x punto en el que se aproximará la Hessiana de f
+  # Out:
+  # Hf: aproximación a la Hessiana de f por diferencias hacia delante
+  
+  h      <- 1e-5
+  n      <- length(x)
+  Hf     <- matrix(rep(0, n*n), nrow = n)
+  xaux_i <- x
+  xaux_j <- x
+  
+  for(i in 1:n) {
+    
+    xaux_i[i] <- xaux_i[i] + h
+    xaux_ij   <- x
+    
+    for(j in i:n) {
+      
+      xaux_j[j]  <- xaux_j[j] + h
+      xaux_ij[j] <- xaux_i[j] + h
+      dif1       <- f(xaux_ij) - f(xaux_i) 
+      dif2       <- f(xaux_j) - f(x)
+      Hf[i,j]    <- (dif1 - dif2) / (h * h)
+      
+      if(j != i) Hf[j,i] <- (dif1 - dif2) / (h * h)
+      
+      xaux_j[j]  <- xaux_j[j] - h
+      xaux_ij[j] <- xaux_ij[j] - h
+      
+    }
+    
+    xaux_i[i] <- xaux_i[i] - h
+    
+  }
+  
+  return(Hf)
+  
+}
+
+
+backtracking <- function(alpha, beta, f, dir_desc, x, derivada_direccional) {
+  
+  # In:
+    # alpha, beta parámetros del método por backtracking
+    # f función de Rn a R a minimizar
+    # dir_desc dirección de descenso en el punto x
+    # x punto en el que se buscará el tamaño de paso para la dirección de descenso dada
+    # grad_f gradiente de f en x
+  # Out: 
+    # t tamaño de paso
+  
+  t <- 1
+  
+  if(alpha > 0.5) {
+    print('alpha de backtracking debe ser menor o igual a 1/2')
+    t <-  -1
+  }
+  
+  if(beta > 1) {
+    print('beta de backtracking debe ser menor a 1')
+    t <-  -1
+  }
+  
+  if(t != -1) {
+    
+    eval1 <- f(x + t * dir_desc)
+    eval2 <- f(x) + alpha * t * derivada_direccional
+    
+    while (eval1 > eval2) {
+      
+      t     <- beta * t
+      eval1 <- f(x + t * dir_desc)
+      eval2 <- f(x) + alpha * t * derivada_direccional
+      
+    }
+    
+  } else {
+    
+    t <- -1
+    
+  }
+  
+  return(t)
+  
+}
+
+newton_Axib <- function(f, A, b, x_ast, p_ast, x0, tol, tol_backtracking, maxiter) {
+  
+  # Entrada:
+    # f: función a encontrar el mínimo. Definida como una function handle
+    # A y b: matriz y lado derecho del conjunto de restricciones de igualdad del problema
+    # de optimización (Ax = b)
+    # x_ast: solución de min f(x) sujeto a: Ax=b.
+    # p_ast: valor óptimo de f: p_ast = f(x_ast)
+    # x0: aproximaciones iniciales a x_ast (mínimo de f) para los algoritmos
+    # tol: para el criterio de paro. Típicamente menor o igual a 1e-8. Controla decremento en x 
+    # tol_backtracking: para backtracking. Típicamente menor o igual a 1e-14. Controla actualización de x.
+    # maxiter: máximo número de iteraciones a realizar.
+  # Salida:
+    # x: aproximación a x_ast
+    # iter: número de iteraciones realizadas (para gráficas de monitoreo)
+    # Err_plot: error medido como error absoluto o relativo respecto a p_ast (para gráficas de monitoreo)
+    # x_plot: vector de aproximaciones (para gráficas de monitoreo)
+  
+  
+  
+  return(list(x = x, iter = iter, Err_plot = Err_plot, x_plot = x_plot))
+  
+}
+
+
+fx <- function(x) {
+  sum(x^2)
+}
+
+gradiente(fx, c(2,2))
+hessiana(fx, c(2,2))
+
+
+
